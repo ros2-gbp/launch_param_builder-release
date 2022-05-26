2fc753ad0e1019842dd8a41cad0b92e56658e7c7
JafarAbdi <cafer.abdi@gmail.com>
2022-05-26 14:42:19 +0000

    Rebase from 'upstream'

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ package_name = "launch_param_builder"
 
 setup(
     name=package_name,
-    version="0.1.0",
+    version="0.1.1",
     packages=[package_name],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         (
             "share/" + package_name + "/launch",
-            ["example/launch_param_builder_example.launch.py"],
+            [
+                "example/launch_param_builder_example.launch.py",
+                "example/parameter_injection_example.launch.py",
+            ],
         ),
         ("share/" + package_name + "/config", glob("example/config/*")),
         ("share/" + package_name, ["package.xml"]),
